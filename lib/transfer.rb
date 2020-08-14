724096676996801e6e860214b8371e7003956a24
Christie Yoon <cleex1999@gmail.com>
2020-08-14 04:35:06 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -31,9 +31,14 @@ class Transfer
   end
 
   def reverse_transfer
-     if @status = "complete"
-        self.execute_transaction.reverse
-        #binding.pry
-     end
+    if @receiver.balance > @amount and valid? and @status == "complete"
+       #binding.pry
+      @sender.deposit(@amount)
+      @receiver.balance -= @amount
+      return @status = "reversed"
+    else
+     @status = "rejected"
+     return "Transaction rejected. Please check your account balance."
+    end
   end
 end
